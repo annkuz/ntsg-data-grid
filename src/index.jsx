40f6a7bd1be37e2b925cfcffbbd4695e0c005db3
annkuz <jumbie666@yandex.ru>
2018-07-09 20:35:42 +0300

    Added filter feature

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom";
 
 const ReactDataGrid = require('react-data-grid');
 const React = require('react');
+const { Toolbar, Data: { Selectors } } = require('react-data-grid-addons');
 import update from 'immutability-helper';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,41 +15,57 @@ class Example extends React.Component {
             {
                 key: 'id',
                 name: 'ID',
-                width: 80
+                width: 80,
+                sortable: true,
+                filterable: true
             },
             {
                 key: 'task',
                 name: 'Title',
-                editable: true
+                editable: true,
+                sortable: true,
+                filterable: true
             },
             {
                 key: 'priority',
                 name: 'Priority',
-                editable: true
+                editable: true,
+                sortable: true,
+                filterable: true
             },
             {
                 key: 'issueType',
                 name: 'Issue Type',
-                editable: true
+                editable: true,
+                sortable: true,
+                filterable: true
             },
             {
                 key: 'complete',
                 name: '% Complete',
-                editable: true
+                editable: true,
+                sortable: true,
+                filterable: true
             },
             {
                 key: 'startDate',
                 name: 'Start Date',
-                editable: true
+                editable: true,
+                sortable: true,
+                filterable: true
             },
             {
                 key: 'completeDate',
                 name: 'Expected Complete',
-                editable: true
+                editable: true,
+                sortable: true,
+                filterable: true
             }
         ];
+        let originalRows = this.createRows(20);
+        let rows = originalRows.slice(0);
+        this.state = { originalRows, rows, filters: {} };
 
-        this.state = { rows: this.createRows(20) };
     }
 
     getRandomDate = (start, end) => {
@@ -71,8 +88,34 @@ class Example extends React.Component {
         return rows;
     };
 
-    rowGetter = (i) => {
-        return this.state.rows[i];
+
+    getRows = () => {
+        return Selectors.getRows(this.state);
+    };
+
+    getSize = () => {
+        return this.getRows().length;
+    };
+
+
+    rowGetter = (rowIdx) => {
+        let rows = this.getRows();
+        return rows[rowIdx];
+    };
+
+    handleFilterChange = (filter) => {
+        let newFilters = Object.assign({}, this.state.filters);
+        if (filter.filterTerm) {
+            newFilters[filter.column.key] = filter;
+        } else {
+            delete newFilters[filter.column.key];
+        }
+        this.setState({ filters: newFilters });
+    };
+
+    onClearFilters = () => {
+        // all filters removed
+        this.setState({filters: {} });
     };
 
     handleGridRowsUpdated = ({ fromRow, toRow, updated }) => {
@@ -87,15 +130,33 @@ class Example extends React.Component {
         this.setState({ rows });
     };
 
+    handleGridSort = (sortColumn, sortDirection) => {
+        const comparer = (a, b) => {
+            if (sortDirection === 'ASC') {
+                return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+            } else if (sortDirection === 'DESC') {
+                return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+            }
+        };
+
+        const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+
+        this.setState({ rows });
+    };
+
     render() {
         return  (
             <ReactDataGrid
                 enableCellSelect={true}
+                onGridSort={this.handleGridSort}
                 columns={this._columns}
                 rowGetter={this.rowGetter}
-                rowsCount={this.state.rows.length}
+                rowsCount={this.getSize()}
                 minHeight={500}
-                onGridRowsUpdated={this.handleGridRowsUpdated} />);
+                onGridRowsUpdated={this.handleGridRowsUpdated}
+                toolbar={<Toolbar enableFilter={true}/>}
+                onAddFilter={this.handleFilterChange}
+                onClearFilters={this.onClearFilters}/>);
     }
 }
 
